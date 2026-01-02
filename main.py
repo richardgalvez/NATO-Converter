1365fc9a73bbd17eefd5ea246532ec63a2d4a53c
Richard Galvez <richardalbertogalvez@gmail.com>
2026-01-02 01:08:48 -0500

    WIP - Account for numbers/integers.

--- main.py
+++ main.py
@@ -4,14 +4,27 @@ nato_alphabet = ["Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf"
                  "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform"
                  "Victor", "Whiskey", "X-Ray", "Yankee", "Zulu"]
 
+# TODO: Account for numbers/integers
 input_string = input("Enter the text you need to have it converted into the NATO Phonetic Alphabet!\n").upper()
 
-# TODO: Account for numbers/integers
 def convert_to_nato(char: str):
     """
-    Match the provided uppercase character to its ASCII value and output its converted NATO value.
+    Find the corresponding NATO string value in the array based on its index
+    after subtracting its ASCII value from the starting point of 65 (which is 'A').
     """
     return nato_alphabet[ord(char) - 65]
 
+i = 0
+
+# FIX: Output is buggy in terminal
 for letter in input_string:
-    print(convert_to_nato(letter))
+    if letter.isdigit():
+        print(letter, end=", ")
+        if i != len(input_string) - 1:
+            print(letter)
+    else:
+        if i != len(input_string) - 1:
+            print(convert_to_nato(letter), end=", ")
+        else:
+            print(convert_to_nato(letter))
+    i += 1
