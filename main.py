2aca08b11d268d328df91a76aadca1c3ad120711
Richard Galvez <richardalbertogalvez@gmail.com>
2026-01-10 22:52:16 -0500

    Updated to handle numbers.

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 # Array that contains all NATO Phonetic Alphabet strings.
+from typing import List
+
+
 nato_alphabet = ["Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf",
                  "Hotel", "India", "Juliet", "Kilo", "Lima", "Mike", "November",
                  "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform",
                  "Victor", "Whiskey", "X-Ray", "Yankee", "Zulu"]
 
-# TODO: Account for numbers/integers
-input_string = input("Enter the text you need to have it converted into the NATO Phonetic Alphabet!\n").upper()
+input_string = input("Enter the text you need to have it converted into the NATO Phonetic Alphabet!\n")
+output = []
 
 def convert_to_nato(char: str):
     """
@@ -14,17 +17,23 @@ def convert_to_nato(char: str):
     """
     return nato_alphabet[ord(char) - 65]
 
-i = 0
+def display_output(arr: List):
+    i = 0
 
-# FIX: Output is buggy in terminal
-for letter in input_string:
-    if letter.isdigit():
-        print(letter, end=", ")
-        if i != len(input_string) - 1:
-            print(letter)
-    else:
-        if i != len(input_string) - 1:
-            print(convert_to_nato(letter), end=", ")
+    for index in arr:
+        if i != len(arr) - 1:
+            print(index, end=", ")
         else:
-            print(convert_to_nato(letter))
-    i += 1
+            print(index)
+        i += 1
+
+# TODO: Handle special characters
+
+for char in input_string:
+    if char.isnumeric():
+        output.append(str(char))
+    elif char.isalpha():
+        converted_char = convert_to_nato(char.upper())
+        output.append(converted_char)
+
+display_output(output)
